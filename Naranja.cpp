bad22648d71097dd88e7d639a386ac7fb3d7be33
SaidCastell <a01569162@tec.mx>
2025-05-23 17:49:23 -0600

    Update Naranja.cpp

--- Naranja.cpp
+++ Naranja.cpp
@@ -1,1 +1,18 @@
-
+#include "Naranja.h"
+#include <iostream>
+using namespace std;
+Naranja::Naranja(): Frutero(){
+    tipo = "Sin datos";
+}
+Naranja::Naranja(string col, string tip): Frutero(col){
+    tipo = tip; 
+}
+void Naranja::setTipo(string tip){
+    tipo = tip; 
+}
+string Naranja::getTipo(){
+    return tipo; 
+}
+void Naranja::ShowFruit(){
+    cout << "Esto es una naranja.";
+}
