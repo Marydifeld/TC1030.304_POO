ca51e472666854dee4dd57a3fad54c3561db45e2
SaidCastell <a01569162@tec.mx>
2025-05-23 11:45:47 -0600

    Update Naranja.h

--- Naranja.h
+++ Naranja.h
@@ -1,1 +1,25 @@
+#ifndef NARANJA_H
+#define NARANJA_H
+#include "Frutero.h"
 
+#include <string>
+
+//Sin terminar
+using namespace std;
+
+class Naranja : public Frutero {
+    private:
+        string carrera;
+
+    public:
+        Naranja(float peso);
+        virtual ~Naranja();
+
+
+        virtual string sabor();
+
+        
+
+};
+
+#endif
