88ed6c8f501c51641f1c51bbf31ed7c6263cb7bd
Lizzeth Rodríguez <lizzeth.rdguez@gmail.com>
2025-05-23 20:44:46 -0600

    nombre Lizz

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
+// Maryfer
+// Said
+// Jessica Lizzeth Rodríguez García A00839775
+
 #include "Naranja.h"
 #include "Manzana.h"
 #include<iostream>
+
 using namespace std;
+
 int main(){
     Frutero* arr[3];
     arr[0] = new Manzana();
